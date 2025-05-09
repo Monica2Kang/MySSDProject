06afc596aec4af3f554499c3760face64c229c74
Monica2Kang <aksenwkd92@naver.com>
2025-05-10 02:05:58 +0900

    [SSD] checkParsing 함수의 테스트 코드와 동작 구현

--- SSD/SSDCmdParser_test.cpp
+++ SSD/SSDCmdParser_test.cpp
@@ -1,5 +1,75 @@
 #include "gmock/gmock.h"
+#include "SSDCmdParser.h"
+#include <string>
 
-TEST(SSDCmdParser, TC1) {
-	EXPECT_EQ(1, 1);
+using std::string;
+
+TEST(SSDCmdParser, ReadSuccess) {
+    SSDCmdParser parser;
+    const char* argv[] = {"SSD.exe", "R", "0"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    string actual = parser.checkParsing(argc, argv);
+    string expected = "0x00000000";
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(SSDCmdParser, ReadFailInvalidLBA) {
+    SSDCmdParser parser;
+    const char* argv[] = { "SSD.exe", "R", "100" };
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    string actual = parser.checkParsing(argc, argv);
+    string expected = "ERROR";
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(SSDCmdParser, WriteSuccess) {
+    SSDCmdParser parser;
+    const char* argv[] = { "SSD.exe", "W", "0", "0x12345678"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    string actual = parser.checkParsing(argc, argv);
+    string expected = "";
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(SSDCmdParser, WriteFailInvalidLBA) {
+    SSDCmdParser parser;
+    const char* argv[] = { "SSD.exe", "W", "100", "0x12345678" };
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    string actual = parser.checkParsing(argc, argv);
+    string expected = "ERROR";
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(SSDCmdParser, WriteFailDecimalValue) {
+    SSDCmdParser parser;
+    const char* argv[] = { "SSD.exe", "W", "0", "12345678" };
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    string actual = parser.checkParsing(argc, argv);
+    string expected = "ERROR";
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(SSDCmdParser, WriteFailBigValue) {
+    SSDCmdParser parser;
+    const char* argv[] = { "SSD.exe", "W", "0", "0x1FFFFFFFF" };
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    string actual = parser.checkParsing(argc, argv);
+    string expected = "ERROR";
+    EXPECT_EQ(expected, actual);
+}
+
+TEST(SSDCmdParser, WriteFailSmallValue) {
+    SSDCmdParser parser;
+    const char* argv[] = { "SSD.exe", "W", "0", "0x1" };
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    string actual = parser.checkParsing(argc, argv);
+    string expected = "ERROR";
+    EXPECT_EQ(expected, actual);
 }
--- SSD/main.cpp
+++ SSD/main.cpp
@@ -1,6 +1,15 @@
 #include "gmock/gmock.h"
+#include "SSDCmdParser.h"
+#include <iostream>
 
-int main() {
+int main(int argc, const char* argv[]) {
+#ifndef _DEBUG
+	SSDCmdParser parser;
+	string result = parser.checkParsing(argc, argv);
+	std::cout << result << "\n";
+	return 0;
+#else
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
+#endif // !1
 }
